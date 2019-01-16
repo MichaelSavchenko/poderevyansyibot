b2dab34259efcef3a5e6f60cf5fc5db4a0718e48
Michael Savchenko <michaelsav4enko@gmail.com>
2019-01-16 20:19:14 +0200

    Hamlet, Tsikavi_Doslidy, Heroj_Nashogo_Chasu

--- src/main/java/com/mihadev/poderevjanskyibot/Book.java
+++ src/main/java/com/mihadev/poderevjanskyibot/Book.java
@@ -4,7 +4,10 @@ public enum Book {
     DANKO("Данко"),
     PACAVATA_ISTORIA ("Пацавата історія"),
     HVOROBA_IVASYKA("Хвороба Івасика"),
-    JAN_MARE("Жан Маре");
+    JAN_MARE("Жан Маре"),
+    HAMLET("Гамлєт, або Феномен датського кацапізма."),
+    TSIKAVI_DOSLIDY("Цікаві досліди"),
+    HEROJ_NASHOGO_CHASU("Герой нашого часу");
 
     Book(String bookName) {
         this.bookName = bookName;
