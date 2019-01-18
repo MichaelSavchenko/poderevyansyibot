1137b74da9c781f5c0d9e389fea62f0c72a0780b
Michael Savchenko <michaelsav4enko@gmail.com>
2019-01-18 23:03:39 +0200

    fix port issue

--- src/main/java/com/mihadev/poderevjanskyibot/LesBotApplication.java
+++ src/main/java/com/mihadev/poderevjanskyibot/LesBotApplication.java
@@ -2,6 +2,7 @@ package com.mihadev.poderevjanskyibot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
