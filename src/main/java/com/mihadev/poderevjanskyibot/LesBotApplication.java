d1a514e14452505c85d973f5e09e9d972fb26c7a
Michael Savchenko <michaelsav4enko@gmail.com>
2019-02-02 13:49:24 +0200

    remove twitter bot

--- src/main/java/com/mihadev/poderevjanskyibot/LesBotApplication.java
+++ src/main/java/com/mihadev/poderevjanskyibot/LesBotApplication.java
@@ -1,9 +1,7 @@
 package com.mihadev.poderevjanskyibot;
 
-import com.mihadev.poderevjanskyibot.twitter.TwitterController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
